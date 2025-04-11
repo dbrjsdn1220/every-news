9f4395ecd4eb474d78dea8b0e29d45e0acb16b8b
유건우 <dbrjsdn1220@gmail.com>
2025-04-11 15:26:16 +0900

    오 마이 뉴스 RSS 추가 및 db 저장 기능 구현

--- news_crawling.py
+++ news_crawling.py
@@ -17,7 +17,12 @@ conn = psycopg2.connect(
     )
 cur = conn.cursor()
 
+# RSS 피드 URL (예: Khan 뉴스 RSS)
+RSS_FEED_URL1 = "https://www.khan.co.kr/rss/rssdata/total_news.xml"
+RSS_FEED_URL2 = "https://rss.ohmynews.com/rss/ohmynews.xml"
 
+
+# 뉴스 데이터를 db로 저장하는 함수
 def save_to_db(data):
     insert_query = """
     INSERT INTO news_article (title, writer, write_date, category, content, url)
@@ -36,15 +41,15 @@ def save_to_db(data):
     conn.commit()
 
 
-# RSS 피드 URL (예: Khan 뉴스 RSS)
-RSS_FEED_URL = "https://www.khan.co.kr/rss/rssdata/total_news.xml"
-
+# 작성자 정보 전처리하는 함수
 def clean_writer(raw_writer):
     # '기자'를 기준으로 자르고 공백 제거
     if "기자" in raw_writer:
         return raw_writer.split("기자")[0].strip()
     return raw_writer.strip()
 
+
+# 날짜 형식 전처리하는 함수
 def format_date(iso_date_str):
     try:
         dt = datetime.fromisoformat(iso_date_str)
@@ -52,6 +57,16 @@ def format_date(iso_date_str):
     except Exception:
         return "날짜 형식 오류"
     
+
+# description을 깔끔하게 글자만 추출하는 함수
+def clean_description(raw_html):
+    soup = BeautifulSoup(raw_html, "html.parser")
+    text = soup.get_text(strip=True)
+    trimmed_text = text.split("전체 내용보기")[0].strip()
+    return trimmed_text
+
+
+# 뉴스 내용 크롤링
 def crawl_article(url):
     headers = {
         "User-Agent": "Mozilla/5.0"
@@ -66,24 +81,42 @@ def crawl_article(url):
     
     content_text = "\n\n".join([tag.text.strip() for tag in content_tag])
     return content_text
+    
 
+# 메인 함수
 def main():
-    print('RSS 피드를 확인하는 중 ...')
-    feed = feedparser.parse(RSS_FEED_URL)
+    print('경향신문 RSS 피드를 확인하는 중 ...')
+    feed = feedparser.parse(RSS_FEED_URL1)
     
     for entry in feed.entries:
         title = entry.get("title", '제목없음')
         writer = clean_writer(entry.get("author", "작성자 없음"))
         write_date = format_date(entry.get("date", "날짜 없음"))
         category = entry.get("category", "카테고리 없음")
+        content = crawl_article(entry.link)
         url = entry.link
-        content = crawl_article(url)
-        print(url)
-        print(title)
-        print(writer)
-        print(write_date)
-        print(category)
-        print(content)
+
+        # DB에 저장
+        data = {
+            "title": title,
+            "writer": writer,
+            "write_date": write_date if write_date != "날짜 형식 오류" else None,
+            "category": category,
+            "content": content,
+            "url": url
+        }
+        save_to_db(data)
+
+    print('오 마이 뉴스 RSS 피드를 확인하는 중 ...')
+    feed = feedparser.parse(RSS_FEED_URL2)
+
+    for entry in feed.entries:
+        title = entry.get("title", '제목없음')
+        writer = entry.get("author", "작성자 없음")
+        write_date = format_date(entry.get("published", "날짜 없음"))
+        category = entry.get("category", "카테고리 없음")
+        content = clean_description(entry.get("description", ""))
+        url = entry.link
 
         # DB에 저장
         data = {
