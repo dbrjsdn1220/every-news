87c4385257242d1420e4d424d30d523d4173cd60
유건우 <dbrjsdn1220@gmail.com>
2025-04-11 11:33:03 +0900

    초기세팅

---
+++ rss_test.py
@@ -0,0 +1,14 @@
+import feedparser
+import requests
+from bs4 import BeautifulSoup
+
+# RSS 피드 URL (예: Khan 뉴스 RSS)
+RSS_FEED_URL = "https://www.khan.co.kr/rss/rssdata/total_news.xml"
+
+def main():
+    feed = feedparser.parse(RSS_FEED_URL)
+    print(feed)
+    
+
+if __name__ == "__main__":
+    main()
