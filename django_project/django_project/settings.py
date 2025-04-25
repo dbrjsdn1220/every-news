9d8093bfe0cef8bf6ef2b593e1a3fcdd89387103
유건우 <dbrjsdn1220@gmail.com>
2025-04-25 16:29:20 +0900

    login function

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -145,3 +145,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
